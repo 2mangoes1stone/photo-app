fbd5c2482a93e39a0619fe48f31b8d177f974a9c
Vignesh Natarajan <vigneshnataraj@gmail.com>
2017-04-30 14:45:35 +1000

    Stripe complete for tier 1

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -12,5 +12,10 @@ class Payment < ApplicationRecord
 
   def process_payment
     customer = Stripe::Customer.create email: email, card: token
+
+    Stripe::Charge.create customer: customer.id,
+                            amount: 1000,
+                       description: 'Premium',
+                          currency: 'usd'
   end
 end
